0fa3940290df26add2f0a5e05e14368c0675c003
xanaxhol1c <loram.hoptan@gmail.com>
2025-04-12 20:08:35 +0300

    added speciality model + migrated

--- EDekanat/main/admin.py
+++ EDekanat/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Speciality
 # Register your models here.
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    list_filter = ['name']
+    
--- EDekanat/main/models.py
+++ EDekanat/main/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Speciality(models.Model):
-    name = models.CharField(max=50, unique=True)
-    description = models.CharField(max=100)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=100)
+
 
     class Meta:
         ordering=['name']
+        verbose_name = 'Speciality'
+        verbose_name_plural = 'Specialities'
 
     def __str__(self):
         return self.name
