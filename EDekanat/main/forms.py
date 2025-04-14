599794bc924bd977215652636ce6a70937c3a24f
xanaxhol1c <loram.hoptan@gmail.com>
2025-04-14 14:44:55 +0300

    temp for future form

---
+++ EDekanat/main/forms.py
@@ -0,0 +1,8 @@
+# from django import forms
+# from .models import Student
+# class StudentSearchForm(forms.Form):
+#     class Meta:
+#         model = Student
+#         fields = ['zalikbook', 'first_name', 'last_name', 'middle_name', 'group', 'course', 'speciality', 'email']
+
+    
