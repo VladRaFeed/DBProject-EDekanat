3dd0f863c04df09ad9e514661cc7eb335ed88030
Vitaliy Lehkii <ondihrmang@gmail.com>
2025-04-14 18:03:57 +0300

    conflict fix

--- EDekanat/main/admin.py
+++ EDekanat/main/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Group, DekanatWorkers
+from .models import Group, DekanatWorkers, Speciality
 
 # Register your models here.
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    list_filter = ['name']
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
--- EDekanat/main/models.py
+++ EDekanat/main/models.py
@@ -37,3 +37,15 @@ class DekanatWorkers(models.Model):
     def __str__(self):
         return f"{self.firstname} {self.middlename} {self.lastname}" if self.middlename else f"{self.firstname} {self.lastname}"
     
+    class Speciality(models.Model):
+        name = models.CharField(max_length=50, unique=True)
+        description = models.CharField(max_length=100)
+
+
+    class Meta:
+        ordering=['name']
+        verbose_name = 'Speciality'
+        verbose_name_plural = 'Specialities'
+
+    def __str__(self):
+        return self.name
