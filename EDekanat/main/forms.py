078365bfdae2718077680e5812dae7ca55f3d922
xanaxhol1c <loram.hoptan@gmail.com>
2025-04-15 14:32:42 +0300

    created form for creating requests for documents

--- EDekanat/main/forms.py
+++ EDekanat/main/forms.py
@@ -1,8 +1,27 @@
-# from django import forms
-# from .models import Student
-# class StudentSearchForm(forms.Form):
-#     class Meta:
-#         model = Student
-#         fields = ['zalikbook', 'first_name', 'last_name', 'middle_name', 'group', 'course', 'speciality', 'email']
+from django import forms
+from .models import Group, Document
 
-    
+class StudentSearchForm(forms.Form):
+    zalikbook = forms.CharField(label="Номер залікової книжки")
+    last_name = forms.CharField(label="Прізвище")
+    first_name = forms.CharField(label="Ім’я")
+    middle_name = forms.CharField(label="По-батькові")
+
+    group = forms.ModelChoiceField(
+        label='Група',
+        queryset=Group.objects.all(),
+        empty_label='Оберіть групу',
+    )
+
+    requesteddocument = forms.ModelChoiceField(
+        label='Бажаний документ',
+        queryset=Document.objects.all(),
+        empty_label='Оберіть документ',
+    )
+    # course = forms.ModelChoiceField(
+    #     label='Курс',
+    #     queryset=Course.objects.all(),
+    #     empty_label='Оберіть курс',
+    # )
+    # email = forms.EmailField(required=False) — лише для повідомлень
+
--- EDekanat/main/views.py
+++ EDekanat/main/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
+from .forms import StudentSearchForm
+from .models import Student, Group, Requests
 
-namespace = 'main'
-# Create your views here.
 def students_form(request):
-    return render(request, 'main/home-form.html')
+    form = StudentSearchForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        zalikbook = form.cleaned_data['zalikbook']
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        middle_name = form.cleaned_data['middle_name']
+        group_name = form.cleaned_data['group']
+
+        group = form.cleaned_data['group']
+        if not group:
+            form.add_error('group', 'Групу не знайдено.')
+        else:
+
+            student = Student.objects.filter(
+                zalikbook=zalikbook,
+                firstname__iexact=first_name,
+                lastname__iexact=last_name,
+                middlename=middle_name,
+                groupid=group
+            ).first()
+
+            if student:
+                requested_document = form.cleaned_data['requesteddocument']
+                Requests.objects.create(
+                    student=student,
+                    requested_document=requested_document
+                )
+                return render(request, 'main/home-form.html', {
+                    'form': form,
+                    'student': student
+                })
+
+    return render(request, 'main/home-form.html', {'form': form})
